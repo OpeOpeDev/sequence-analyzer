766d81b8ffc893d0f6cf049dfef51611196e5868
crazyfullstack <mattsonjohnny2@gmail.com>
2024-08-26 20:02:51 -0400

    changed generated file size from 100MB to 10MB

--- bin/file_generator_cli.rb
+++ bin/file_generator_cli.rb
@@ -7,7 +7,7 @@ file2_path = 'data/sense_and_sensibility.txt'
 output_path = 'data/large_test_file.txt'
 
 # You can specify the target file size in MB and the number of sentences here
-target_size_mb = 100  # Set desired file size in MB
+target_size_mb = 10  # Set desired file size in MB
 num_sentences = 100_000  # Number of sentences to generate
 
 LargeFileGenerator.generate_large_file(file1_path, file2_path, output_path, target_size_mb, num_sentences)
